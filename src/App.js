b683f844e48235ea75898c31682fbd43b7fe62fe
Cerebrovinny <vinny.cardoso96@gmail.com>
2021-01-10 08:30:51 -0300

    React component part 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
 
+import Menu from './components/MenuComponent';
+
 function App() {
   return (
     <div className="App">
@@ -9,6 +11,7 @@ function App() {
           <NavbarBrand href="/">Ristorante Confusion</NavbarBrand>
         </div>
       </Navbar>
+      <Menu />
     </div>
   );
 }
