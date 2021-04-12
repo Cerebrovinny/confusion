fcb35690d9ec5b04d7e4c2788d73079101475f78
Cerebrovinny <vinny.cardoso96@gmail.com>
2021-04-11 23:37:13 -0300

    Added assigment 2

--- src/App.js
+++ src/App.js
@@ -1,31 +1,19 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import Menu from './components/MenuComponent';
-import { DISHES } from './shared/dishes'
+import Main from './components/MainComponent';
+import './App.css';
+import { BrowserRouter } from 'react-router-dom';
 
 class App extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            dishes: DISHES
-        };
-    }
-
-    render() {
-        return (
-            <div className='App'>
-                <Navbar dark color="primary">
-                    <div className="container">  <NavbarBrand href="#">
-                        Vaidic Joshi
-                        </NavbarBrand>
-                    </div >
-                </Navbar>
-                <Menu dishes={this.state.dishes} />
-            </div >
-
-        );
-    }
+  render() {  
+    return (
+        <BrowserRouter>
+            <div className="App">
+                <Main /> 
+            </div>
+        </BrowserRouter>
+    );
+  }
 }
 
 export default App;
